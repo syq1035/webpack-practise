92f67d9882a2b028a0a7c94fdfdd239b97beedd5
Yanqin Shen <yanqin.shen@thoughtworks.com>
2022-09-06 18:24:16 +0800

    add webpack base config

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js', // 打包入口地址
+  output: {
+    filename: 'bundle.js', // 输出文件名
+    path: path.join(__dirname, 'dist') // 输出文件目录
+  }
+}
