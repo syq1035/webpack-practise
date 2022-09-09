0a9e2bbb33e999a40bfdc13af59f5a3aafb3801d
Yanqin Shen <yanqin.shen@thoughtworks.com>
2022-09-09 15:19:35 +0800

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.css', // 打包入口地址
+  entry: './src/index.js', // 打包入口地址
   output: {
     filename: 'bundle.js', // 输出文件名
     path: path.join(__dirname, 'dist') // 输出文件目录
   },
   module: {
-    rules: [ // 转换规则
+    rules: [
       {
         test: /\.css$/, //匹配所有的 css 文件
         use: 'css-loader' // use: 对应的 Loader 名称
       }
     ]
-  }
+  },
+  plugins:[ // 配置插件
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
