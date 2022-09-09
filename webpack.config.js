941d11a4e5db00e0f105171f3998438caf63d900
Yanqin Shen <yanqin.shen@thoughtworks.com>
2022-09-09 18:04:35 +0800

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,21 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 console.log('process.env.NODE_ENV =', process.env.NODE_ENV) // 打印环境变量
 
-module.exports = {
+const config = {
   entry: './src/index.js', // 打包入口地址
   output: {
     filename: 'bundle.js', // 输出文件名
     path: path.join(__dirname, 'dist') // 输出文件目录
+  },
+  devServer: {
+    allowedHosts: 'all',
+    static: {
+      directory: path.join(__dirname, 'public'),
+    }, // 静态文件目录
+    compress: true, //是否启动压缩 gzip
+    port: 3000, // 端口号
+    open: true,  // 是否自动打开浏览器
+    hot: false, //为了 liveReload 能够生效，devServer.hot 配置项必须禁用
   },
   module: {
     rules: [
@@ -18,10 +28,12 @@ module.exports = {
       }
     ]
   },
-  plugins:[ // 配置插件
+  plugins: [ // 配置插件
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
     new CleanWebpackPlugin() // 引入插件
   ]
 }
+
+module.exports = config
