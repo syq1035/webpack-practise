f79ac5ea7478df5ab784932751e91d1611cd5df0
Yanqin Shen <yanqin.shen@thoughtworks.com>
2022-09-09 15:11:55 +0800

    add css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js', // 打包入口地址
+  entry: './src/index.css', // 打包入口地址
   output: {
     filename: 'bundle.js', // 输出文件名
     path: path.join(__dirname, 'dist') // 输出文件目录
+  },
+  module: {
+    rules: [ // 转换规则
+      {
+        test: /\.css$/, //匹配所有的 css 文件
+        use: 'css-loader' // use: 对应的 Loader 名称
+      }
+    ]
   }
 }
